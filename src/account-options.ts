341661fd898d5a75a0053eea385181565d062564
not_a_seagull <notaseagull048@gmail.com>
2019-12-14 13:40:33 -0800

    Account options button

--- src/account-options.ts
+++ src/account-options.ts
@@ -1,5 +1,5 @@
-/*!
- * _entry.ts
+/*
+ * account-options.ts
  *
  * scipnet - Frontend scripts for mekhane
  * Copyright (C) 2019 not_a_seagull
@@ -18,27 +18,32 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// note: this is called _entry.ts so it comes first in the browserify bundle
-// this way, it's better to setup global polyfills here than anywhere else
-
-// imports from core-js, should polyfill out most standards
-import "core-js/stable";
-
-// promise polyfill, if needed, will be put here
-
+// assigns trigger to the account options button
 import * as $ from "jquery";
 import * as BluebirdPromise from "bluebird";
 
-import setupAccountOptions from "./account-options";
-import setupAuth from "./login";
-import setupMarkdown from "./markdown";
-import setupPageUtils from "./page-utils";
+export default function setupAccountOptions() {
+  const accountButton = $("#account-topbutton");
+  if (accountButton.length) {
+    const accountOptions = $("#account-options");
+   
+    let currentlyDisplaying = false;
 
-// document onload
-$(() => {
-  console.log("Initialize SCIPNET onload scripts...");
-  setupAccountOptions();
-  setupMarkdown();
-  setupAuth();
-  setupPageUtils();
-});
+    const showAccountOptions = () => {
+      accountOptions.css("display", "block");
+      currentlyDisplaying = true;
+      accountButton.off("click");
+    };
+ 
+    // hide options when no longer hovering
+    // TODO: mobile support
+    accountOptions.hover(() => {}, () => {
+      if (currentlyDisplaying) {
+        accountOptions.css("display", "none");
+        currentlyDisplaying = false;
+        accountButton.click(showAccountOptions);
+      }
+    });
+    accountButton.click(showAccountOptions);
+  }
+}
